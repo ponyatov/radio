0f7caac3cb92d8eb3320bee7cd77684678193b34
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-14 02:11:28 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/sync.mk
+++ mk/sync.mk
@@ -1,3 +1,7 @@
 .PHONY: sync
 sync: doc
 	rsync -r $(HOME)/.config/sdrpp/ etc/sdrpp/
+
+.PHONY: unsync
+unsync:
+	rsync -r etc/sdrpp/frequency_manager_config.json $(HOME)/.config/sdrpp/frequency_manager_config.json
