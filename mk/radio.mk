f01c18b4196d2e5a273e8f700935ed8b0df1bd27
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-13 20:52:52 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/radio.mk
+++ mk/radio.mk
@@ -1,6 +1,14 @@
+.PHONY: radio
+radio: ref
+	sudo update-initramfs -u
+
 RF += /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
 /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf:
 	echo 'blacklist dvb_usb_rtl28xxu' | sudo tee -a $@
 RF += /etc/modprobe.d/blacklist-rtl2832.conf
 /etc/modprobe.d/blacklist-rtl2832.conf:
 	echo 'blacklist rtl2832' | sudo tee -a $@
+
+RF += ref/rtl-sdr/README.md
+ref/rtl-sdr/README.md:
+	$(GITREF) https://github.com/osmocom/rtl-sdr.git $(dir $@)
